58e580a0ec041a18d403922d3d50f1e49fccd95f
freshes <forstudying94@gmail.com>
2026-06-14 17:11:34 +0500

    newma

--- WebApplication9/Program.cs
+++ WebApplication9/Program.cs
@@ -7,7 +7,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-app.UseHttpsRedirection();
+
 app.MapGet("/app/markus_kek_mail_ru", (string? x, string? y) =>
 {
     if (!long.TryParse(x, out long numx) || !long.TryParse(y, out long numy) || numx < 0 || numy < 0)
@@ -19,5 +19,5 @@ app.MapGet("/app/markus_kek_mail_ru", (string? x, string? y) =>
     return Results.Text(lcm.ToString());
 });
 static long CalculateGCD(long a, long b) => b == 0 ? a : CalculateGCD(b, a % b);
-static long CalculateLCM(long a, long b) => (a * b) / CalculateGCD(a, b);
+static long CalculateLCM(long a, long b) => (a / CalculateGCD(a, b)) * b;
 app.Run();
